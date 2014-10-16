df2884a45aaed7921004fd3b748074df36fa12a0
Patrick Bennett <patrickisgreat@gmail.com>
2014-10-16 12:39:57 -0400

    Update buildIt.php

--- buildIt.php
+++ buildIt.php
@@ -2,7 +2,7 @@
 
 require_once('buildRadiansClass.php');
 
-$buildRadians = new buildRadians('162.243.217.180', 'pbennett', 'swacuGaKur2j', 'vbulletin');
+$buildRadians = new buildRadians('db.host', 'dbuser', 'dbPassword', 'dbTable');
 
 $buildRadians->buildRadians();
 
